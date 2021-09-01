2e64793bd25685d4d988c25bd6ba15205a6f9f4f
Matthew Pierce <lesbianhaircut@outlook.com>
2021-09-01 16:33:28 -0400

    c

--- script.js
+++ script.js
@@ -1,18 +1,40 @@
+// Imports
+
+import { colorVariables, darkColors, lightColors } from "./variables.js";
 window.onload = () => {
   // Variables
   const darkIcon = document.querySelector(".toggle-icon");
   const toggleCircle = document.querySelector(".toggle-circle");
+
+  // Iterates through list of color variables and colors to change them to using document root
+  function setDarkMode() {
+    for (let colorIndex in colorVariables) {
+      document.documentElement.style.setProperty(
+        colorVariables[colorIndex],
+        darkColors[colorIndex]
+      );
+    }
+  }
+  // Iterates through list of color variables and colors to change them to using document root
+  function setLightMode() {
+    for (let colorIndex in colorVariables) {
+      document.documentElement.style.setProperty(
+        colorVariables[colorIndex],
+        lightColors[colorIndex]
+      );
+    }
+  }
 
   // Functions
   function toggleDarkMode() {
     if (darkIcon.classList.contains("color-toggle")) {
       darkIcon.classList.remove("color-toggle");
       toggleCircle.classList.remove("float-left");
-      setDarkMode();
+      setLightMode();
     } else {
       darkIcon.classList.add("color-toggle");
       toggleCircle.classList.add("float-left");
-      setLightMode();
+      setDarkMode();
     }
   }
 
---
+++ variables.js
@@ -0,0 +1,27 @@
+const colorVariables = [
+  "--background-color",
+  "--top-background-color",
+  "--card-background-color",
+  "--heading-color",
+  "--text-content-color",
+  "--toggle-color",
+];
+const darkColors = [
+  "hsl(230,17%, 14%)",
+  "hsl(232, 19%, 15%)",
+  "hsl(228, 28%, 20%)",
+  "hsl(228, 34%, 66%)",
+  "hsl(0, 0%, 100%)",
+  "linear gradient hsl(210, 78%, 56%) to hsl(146, 68%, 55%)",
+];
+
+const lightColors = [
+  "hsl(0, 0%, 100%)",
+  "hsl(225, 100%, 98%)",
+  "hsl(227, 47%, 96%)",
+  "hsl(228, 12%, 44%)",
+  "hsl(230, 17%, 14%)",
+  "hsl(230, 22%, 74%)",
+];
+
+export { colorVariables, darkColors, lightColors };
